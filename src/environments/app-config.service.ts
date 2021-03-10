c76c2e16010d2d9cabf9086b5ce54db17139ebf8
Senthilkumar <psysenthilkumar@gmail.com>
2021-03-10 15:28:27 +0530

    first commit

---
+++ src/environments/app-config.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IAppConfig } from './i-app-config';
+
+@Injectable()
+export class AppConfigService {
+
+    ENVIRONMENTS = {
+        QA: {ENV_NAME:'QA', URL:'d9143859-gitfocusns-gitfoc-3d5c-2102416299.ap-south-1.elb.amazonaws.com/', REG:/^d9143859-gitfocusns-gitfoc-3d5c-2102416299.ap-south-1.elb.amazonaws.com/},
+        PROD: {ENV_NAME:'PROD', URL:'a5c4d7aa7937a47199215a5d99482976-1868549657.ap-south-1.elb.amazonaws.com', REG:/^a5c4d7aa7937a47199215a5d99482976-1868549657.ap-south-1.elb.amazonaws.com/}
+    }
+
+    static setting : IAppConfig;
+    
+    envConfigFilePrefix = "/assets/config/app-config-";
+    envConfigFileExtn = ".json";
+
+    constructor(private http: HttpClient) {}
+ 
+    load() {
+        return new Promise<void>((resolve, reject) => {
+            let configEnvFileName = this.envConfigFilePrefix + this.getENV() + this.envConfigFileExtn;
+            this.http.get(configEnvFileName).toPromise().then((response : IAppConfig) => {
+               AppConfigService.setting = <IAppConfig>response;
+               resolve();
+            }).catch((response: any) => {
+               reject(`Could not load the config file`);
+            });
+        });
+    }
+
+    private getENV(): string {
+        const hostname = window && window.location && window.location.hostname;
+        let ENV = "";
+        if (this.ENVIRONMENTS.QA.REG.test(hostname)) {
+            ENV = this.ENVIRONMENTS.QA.ENV_NAME;
+        } else if (this.ENVIRONMENTS.PROD.REG.test(hostname)) {
+            ENV = this.ENVIRONMENTS.PROD.ENV_NAME;
+        }
+
+        return ENV;
+      } 
+} 
